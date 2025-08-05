7535a77b3da642fd0333ece318f31c3eadfd4b4b
Victor Patru <patruvictor.mihai@gmail.com>
2025-08-05 14:01:43 +0300

    fix: sentry leftover

--- apps/app/src/app/global-error.tsx
+++ apps/app/src/app/global-error.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
 import NextError from "next/error";
 import { useEffect } from "react";
 
@@ -10,7 +9,7 @@ export default function GlobalError({
   error: Error & { digest?: string };
 }) {
   useEffect(() => {
-    Sentry.captureException(error);
+    // Sentry.captureException(error);
   }, [error]);
 
   return <NextError statusCode={0} />;
