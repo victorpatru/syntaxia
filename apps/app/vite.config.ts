d81b229092863201d032dc3f5d26fc918c2ab1ae
Victor Patru <patruvictor.mihai@gmail.com>
2025-08-07 11:51:44 +0300

    feat: add tanstack start + clerk scaffolding

---
+++ apps/app/vite.config.ts
@@ -0,0 +1,19 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tanstackStart({
+      customViteReactPlugin: true,
+    }),
+    react(),
+  ],
+});
