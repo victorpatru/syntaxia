5eb5259ddd8fd45f5aee4e335229cee68556852d
Victor Patru <patruvictor.mihai@gmail.com>
2025-08-11 15:51:10 +0300

    fix: remove old remnants of convex auth

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -1,21 +1,15 @@
-import { authTables } from "@convex-dev/auth/server";
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  ...authTables,
   users: defineTable({
-    // Convex Auth fields
-    name: v.optional(v.string()),
-    image: v.optional(v.string()),
+    clerkId: v.string(),
     email: v.optional(v.string()),
-    emailVerificationTime: v.optional(v.number()),
-    phone: v.optional(v.string()),
-    phoneVerificationTime: v.optional(v.number()),
-    isAnonymous: v.optional(v.boolean()),
-
-    // custom fields
+    name: v.optional(v.string()),
     username: v.optional(v.string()),
+    image: v.optional(v.string()),
     imageId: v.optional(v.id("_storage")),
-  }).index("email", ["email"]),
+  })
+    .index("by_clerkId", ["clerkId"])
+    .index("by_email", ["email"]),
 });
