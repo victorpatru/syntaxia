83560e6b29b940f49fbd823cda682fe72ca06c2f
Victor Patru <patruvictor.mihai@gmail.com>
2025-08-26 01:20:33 +0300

    feat: register the http route to be able to accept the webhook events from polar

--- packages/backend/convex/http.ts
+++ packages/backend/convex/http.ts
@@ -1,5 +1,6 @@
 import { httpRouter } from "convex/server";
 import { handleClerkWebhook } from "./http/clerk";
+import { handlePolarWebhook } from "./http/polar";
 
 const http = httpRouter();
 
@@ -10,6 +11,15 @@ http.route({
   path: "/webhook/clerk",
   method: "POST",
   handler: handleClerkWebhook,
+});
+
+/**
+ * Polar webhook endpoint
+ */
+http.route({
+  path: "/webhook/polar",
+  method: "POST",
+  handler: handlePolarWebhook,
 });
 
 export default http;
